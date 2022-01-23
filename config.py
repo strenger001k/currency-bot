8513d93cbd05c3577dbee46e4d829c8387fd7d9f
k0tyra <zhenia0072@gmail.com>
2022-01-23 17:28:23 +0200

    Ok

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 TOKEN = '5040363568:AAFiBh5TmBywj0D5MsgSkWOpvuR1aHb_AmU'
 PRIVAT_API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
-DB_URI = 'postgres://kwluvcgugtkxrx:09965c115473ec494324ccf49d84a02d970b188dc3abbeea67915d6fd28340bb@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dcqspd0jlc3a04'
 ADMIN_ID = 399374025
 ADMIN_CHAT = -776148077
 ADMIN = '@k0tyra'
