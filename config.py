1f12c1622cbfae0837e6a2494ce1d214f1ce83f0
k0tyra <zhenia0072@gmail.com>
2022-01-26 17:45:13 +0200

    new token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '5040363568:AAFR0O7gm_3lJXSY0l3aZ9UBr8caMSFAU64'
+TOKEN = '5040363568:AAHl9E2iGo-s3p42NWNb8ok-KVJ4ViQy4v8'
 PRIVAT_API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 ADMIN_ID = 399374025
 ADMIN_CHAT = -776148077
