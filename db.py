d24a6901ea95432ba5c23804ca11c07a9104ecf5
k0tyra <zhenia0072@gmail.com>
2022-01-26 17:43:53 +0200

    create_user

--- db.py
+++ db.py
@@ -2,13 +2,11 @@ from xlsxwriter.workbook import Workbook
 import messages
 import sqlite3
 
-# conn = psycopg2.connect(DB_URI, sslmode="require")
-# cursor = conn.cursor()
 conn = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 def create_user(user_id: int, username: str, user_name: str, user_surname: str):
-    cursor.execute('INSERT INTO User (user_id, username, user_name, user_surname) VALUES (?, ?, ?, ?)',
+    cursor.execute('INSERT INTO Users (user_id, username, user_name, user_surname) VALUES (?, ?, ?, ?)',
                    (user_id, username, user_name, user_surname))
     conn.commit()
 
