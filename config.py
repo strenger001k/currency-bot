6096270c019c06a1631015cd4b6f8202943f1111
k0tyra <zhenia0072@gmail.com>
2022-01-26 22:55:49 +0200

    new chat

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = '5040363568:AAHl9E2iGo-s3p42NWNb8ok-KVJ4ViQy4v8'
 PRIVAT_API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 ADMIN_ID = 399374025
-ADMIN_CHAT = -776148077
+ADMIN_CHAT = -682720940
 ADMIN = '@k0tyra'
 APP_NAME = 'zhenia-currency-bot'
